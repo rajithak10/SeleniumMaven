5e8f39b8878be17e683ca276f8cfdee461dcc54d
Dell <Dell@DESKTOP-SDHTPGV.attlocal.net>
2023-06-21 14:42:35 -0400

    Commiting add functionality

---
+++ JavaOOPS/src/classwork/AddFunctionality.java
@@ -0,0 +1,15 @@
+package classwork;
+
+public class AddFunctionality {
+
+	
+	public static void main(String[] args) {
+		int result;
+		int a = 10;
+		int b = 5;
+		result = a + b;
+		System.out.println("The result of adding two numbers : " + result);
+
+	}
+
+}
